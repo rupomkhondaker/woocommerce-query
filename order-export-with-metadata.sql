4232fbfc331644bc8ce4fe2c07031b9c819128d3
Rupom Khondaker <rupomkhondaker@users.noreply.github.com>
2023-07-02 02:32:06 +0600

    Update order-export-with-metadata.sql

--- order-export-with-metadata.sql
+++ order-export-with-metadata.sql
@@ -1,12 +1,10 @@
 SELECT
     p.ID AS order_id,
-    CONCAT( cpm2.meta_value ,' ', cpm3.meta_value) AS customer_name,
+    concat(max( CASE WHEN pm.meta_key = '_billing_first_name'  AND p.ID = pm.post_id THEN pm.meta_value END )," ",max( CASE WHEN pm.meta_key = '_billing_last_name'   AND p.ID = pm.post_id THEN pm.meta_value END ) ) as 'Customer Name',
     DATE(p.post_date) AS post_date,
     cpm.meta_value AS currency,
     (SELECT GROUP_CONCAT(order_item_name SEPARATOR '|') FROM wp_filn_woocommerce_order_items WHERE order_id = p.ID) AS order_items,
-    MAX(CASE WHEN pm.meta_key = '_order_tax_rate_id' AND p.ID = pm.post_id THEN (
-    		SELECT meta_value FROM wp_filn_woocommerce_order_itemmeta WHERE order_item_id = oi.order_item_id AND meta_key = 'tax_rate_id'
-	) END) AS tax_name,
+    ( select wtr.tax_rate_name from wp_filn_woocommerce_tax_rates wtr where (max( CASE WHEN pm.meta_key = '_shipping_state' and p.ID = pm.post_id THEN pm.meta_value END )) = wtr.tax_rate_state ) as 'Tax Name',
     MAX(CASE WHEN pm.meta_key = '_order_tax' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_tax,
     DATE(MAX(CASE WHEN pm.meta_key = '_paid_date' AND p.ID = pm.post_id THEN pm.meta_value END)) AS paid_date,
     MAX(CASE WHEN pm.meta_key = '_order_total' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_total
@@ -16,18 +14,15 @@ FROM
     JOIN wp_filn_postmeta cpm ON p.ID = cpm.post_id
     JOIN wp_filn_postmeta pm ON p.ID = pm.post_id
     JOIN wp_filn_woocommerce_order_items oi ON p.ID = oi.order_id
-    JOIN wp_filn_postmeta cpm2 ON p.ID = cpm2.post_id
-    JOIN wp_filn_postmeta cpm3 ON p.ID = cpm3.post_id
 WHERE
     p.post_type = 'shop_order' AND
-    p.post_date BETWEEN '2021-06-01' AND '2021-06-30' AND
+    p.post_date BETWEEN '2022-07-01' AND '2023-07-01' AND
     p.post_status = 'wc-completed' AND 
     cpm.meta_key = '_order_currency' AND
-    cpm.meta_value = 'CAD' AND
-    cpm2.meta_key = '_billing_first_name' AND
-    cpm3.meta_key = '_billing_last_name'
-GROUP BY
-     p.ID;
+    cpm.meta_value = 'CAD'
+
+GROUP BY 
+    order_id;
 //*****------------------------------------------------------
 select
     p.ID as order_id,
